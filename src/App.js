9e787de8feb0f94de891c65fffd9bc0951139b55
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-04-14 20:00:43 +0100

    learned how to create forms in react with uncontrolled and controlled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useReducer } from 'react';
+
+
+const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      firstname: '',
+      lastname: '',
+      gender: '',
+      email: '',
+      address: '',
+      bio: ''
+    }
+  }
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 
 function App() {
+
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    
+
+    setTimeout(() => {
+      setSubmitting(false);
+      setFormData({
+        reset: true
+      })
+    }, 3500);
+  };
+
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Forms</h1>
+      {submitting && 
+      <div>
+        You are submitting the following:
+        <ul>
+          {Object.entries(formData).map(([name, value]) => (
+            <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+          ))}
+        </ul>
+      </div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset disabled={submitting}>
+          <label>
+            <p>First Name</p>
+            <input name="firstname" onChange={handleChange} value={formData.firstname || ''}/>
+          </label>
+
+          <label>
+            <p>Last Name</p>
+            <input name="lastname" onChange={handleChange} value={formData.lastname || ''}/>
+          </label>
+
+          <label>
+            <p>Email</p>
+            <input name="email" onChange={handleChange} value={formData.email || ''}/>
+          </label>
+
+          <label>
+            <p>Gender</p>
+              <select name="gender" onChange={handleChange} value={formData.gender || ''}>
+                  <option value="">--Please choose an option--</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="prefernottosay">Prefer not to say</option>
+              </select>
+          </label>
+
+          <label>
+            <p>Address</p>
+            <input name="address" onChange={handleChange} value={formData.address || ''}/>
+          </label>
+
+          <label>
+            <p>Bio</p>
+            <textarea name="bio" cols="30" rows="5" onChange={handleChange} value={formData.bio || ''}></textarea>
+          </label>
+
+        </fieldset>
+        <button type="submit" disabled={submitting}>Submit</button>
+      </form>
     </div>
   );
 }
